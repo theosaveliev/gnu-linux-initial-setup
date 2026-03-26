fab5bb2df5b1d51e2fae0eb8cf542571e7c77678
Theo Saveliev <89431871+theosaveliev@users.noreply.github.com>
2026-03-27 04:36:12 +0500

    0.4.2 thelounge_users script was added, ngircd config update

---
+++ roles/thelounge_users/scripts/main.py
@@ -0,0 +1,35 @@
+import secrets
+import string
+
+
+def generate_password(length: int) -> str:
+    alphabet = string.ascii_letters + string.digits
+    while True:
+        pw = "".join(secrets.choice(alphabet) for _ in range(length))
+        if (
+            any(c.islower() for c in pw)
+            and any(c.isupper() for c in pw)
+            and any(c.isdigit() for c in pw)
+        ):
+            return pw
+
+
+def main():
+    with open("users", "rt", encoding="utf-8", errors="strict") as fd:
+        users = [line.strip() for line in fd if line.strip()]
+
+    passwords: dict[str, str] = {}
+    for user in users:
+        passwords[user] = generate_password(length=20)
+
+    for user, pw in passwords.items():
+        print(f'    - {{ user: "{user}", password: "{pw}" }}')
+
+    for user, pw in passwords.items():
+        print("[Operator]")
+        print(f"    Name = {user}")
+        print(f"    Password = {pw}")
+
+
+if __name__ == "__main__":
+    main()
